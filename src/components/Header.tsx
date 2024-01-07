22629a6db938991abf8288eef81aaa686e0d6bfe
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2024-01-06 22:06:20 -0300

    ajuste

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,18 +4,16 @@ import Link from "next/link";
 
 export function Header() {
     return (
-        <Box w="100%" bgGradient="linear(to-t, white, blue.400)" flexDirection="row">
-            <Flex p="5" align="center" gap="2">
+        <Box w="100%" bgGradient="linear(to-t, white, blue.400)">
+            <Flex direction="row" p="5" align="center" gap="2">
                 <Heading fontSize={25}>Roberto de Carvalho /</Heading>
                 <Text fontSize={25}>Dev Frontend</Text>
                 <Spacer />
-                
-                <Link href={""}>Sobre mim</Link>
-                
-                <Link href={""}>Projetos</Link>
-                
-                <Link href={""}>Contato</Link>
-                
+                <Flex gap="5" >
+                    <Link href={""}>Sobre mim</Link>
+                    <Link href={""}>Projetos</Link>
+                    <Link href={""}>Contato</Link>
+                </Flex>
             </Flex>
         </Box>
     );
