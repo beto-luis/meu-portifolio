02681e487a3903474af5e9028c8b1c091830424b
Roberto Carvalho <robertodecarvalho0608@gmail.com>
2024-02-11 13:28:42 -0300

    correção na exportação de páginas

---
+++ src/views/index.ts
@@ -0,0 +1,3 @@
+export * from './projetos'
+export * from './skills'
+export * from './sobre_mim' 
--- src/views/sobre_este_portifolio/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex } from "@chakra-ui/react";
-
-export function sobreEstePortifolio() {
-    return (
-        <Flex>
-            
-        </Flex>
-    )
-}
